8ee2704f2cbdcf11a05bae726176c4838de8017b
Mike Sobhy <sobhy0101@gmail.com>
2025-10-22 01:42:57 +0300

    Revamp cookie consent modal UI and logic
    Updated modal styling and positioning for improved appearance. Refactored HTML structure and form fields, added pirate.gif for post-consent feedback, and enhanced JavaScript to handle modal open/close and consent submission with loading and completion states. Removed unused code and streamlined CSS for clarity.

--- courses/07-essential-javaScript/01-build-a-cookie-consent/index.js
+++ courses/07-essential-javaScript/01-build-a-cookie-consent/index.js
@@ -1,17 +1,15 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
-const modalSubmitBtn = document.getElementById('accept-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
-console.log(consentForm)
-console.log("Hello world")
 
 setTimeout(function(){
-    modal.style.display = 'block' // The .style property allows us to access and modify the inline CSS styles of an element. 
-    // Test
-    // modal.style.backgroundColor = '#f0f8ff' // So style is like calling the CSS property directly, then we add the specific property we want to change. In this case, we are changed the background color property to this shade of white.
-    modalCloseBtn.removeAttribute('disabled') // This line removes the disabled attribute from the modalCloseBtn element, enabling the button to be clickable after the modal appears.
+    modal.style.display = 'inline'
 }, 1500)
+
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
@@ -19,17 +17,19 @@ consentForm.addEventListener('submit', function(e){
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
         <p id="upload-text">Uploading your data to the dark web...</p>
-    </div>`
+    </div>` 
+    
     setTimeout(function(){
-        document.getElementById('upload-text').innerText = `Making the sale...`
-    }, 1500) 
-})
-
-// modalCloseBtn.addEventListener('click', function(){
-//     modal.style.display = 'none' // When the close button is clicked, this line sets the display style of the modal element to 'none', effectively hiding it from view.
-// })
-
-// modalSubmitBtn.addEventListener('click', function(event){
-//     modal.style.display = 'none' // Hide the modal
-// })
-
+        document.getElementById('upload-text').innerText = "Making the sale..."
+    }, 1500)
+  
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+        ` 
+    }, 3000)
+}) 
