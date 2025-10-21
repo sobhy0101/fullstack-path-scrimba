d20263dcf1bd2d45aafe041d023b9b19955b0a89
Mike Sobhy <sobhy0101@gmail.com>
2025-10-22 01:15:03 +0300

    Refactor cookie consent modal and form handling
    Merged modal text paragraphs, fixed form and button IDs, and updated event handling in index.js for improved clarity and maintainability. Switched script to module type and formatted loading.svg for readability.

--- courses/07-essential-javaScript/01-build-a-cookie-consent/index.js
+++ courses/07-essential-javaScript/01-build-a-cookie-consent/index.js
@@ -1,7 +1,10 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
-const modalSubmitBtn = document.getElementById('modal-submit-btn')
-const consentForm = document.getElementById('consentForm')
+const modalSubmitBtn = document.getElementById('accept-btn')
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
+console.log(consentForm)
+console.log("Hello world")
 
 setTimeout(function(){
     modal.style.display = 'block' // The .style property allows us to access and modify the inline CSS styles of an element. 
@@ -10,14 +13,23 @@ setTimeout(function(){
     modalCloseBtn.removeAttribute('disabled') // This line removes the disabled attribute from the modalCloseBtn element, enabling the button to be clickable after the modal appears.
 }, 1500)
 
-consentForm.addEventListener('submit', function(event){
-    event.preventDefault()
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>`
+    setTimeout(function(){
+        document.getElementById('upload-text').innerText = `Making the sale...`
+    }, 1500) 
 })
 
-modalCloseBtn.addEventListener('click', function(){
-    modal.style.display = 'none' // When the close button is clicked, this line sets the display style of the modal element to 'none', effectively hiding it from view.
-})
+// modalCloseBtn.addEventListener('click', function(){
+//     modal.style.display = 'none' // When the close button is clicked, this line sets the display style of the modal element to 'none', effectively hiding it from view.
+// })
 
 // modalSubmitBtn.addEventListener('click', function(event){
 //     modal.style.display = 'none' // Hide the modal
 // })
+
