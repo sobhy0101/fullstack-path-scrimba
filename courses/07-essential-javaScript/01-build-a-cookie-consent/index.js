baadc0223efdd78204199ecf555f194a03df9d47
Mike Sobhy <sobhy0101@gmail.com>
2025-10-22 14:33:39 +0300

    Finished '01-build-a-cookie-consent' project
    Added aria-labels to form inputs for better accessibility. Introduced a 'reverse' class to the modal buttons and toggled it on the decline button hover to enhance the user experience.

--- courses/07-essential-javaScript/01-build-a-cookie-consent/index.js
+++ courses/07-essential-javaScript/01-build-a-cookie-consent/index.js
@@ -3,6 +3,7 @@ const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
 const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
 
 setTimeout(function(){
@@ -14,7 +15,7 @@ modalCloseBtn.addEventListener('click', function(){
 })
 
 declineBtn.addEventListener('mouseenter', function(){
-    console.log("hovered")
+    modalChoiceBtns.classList.toggle('reverse')
 })
 
 consentForm.addEventListener('submit', function(e){
