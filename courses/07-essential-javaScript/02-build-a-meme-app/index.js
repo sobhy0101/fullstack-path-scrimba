eab83871eda4383c98b79e89d311280e26cd3753
Mike Sobhy <sobhy0101@gmail.com>
2025-10-23 12:36:55 +0300

    Refactor getEmotionsArray to use for...of loop
    Replaced nested for loops with a for...of loop in getEmotionsArray for improved readability. The function now logs each cat object individually as part of a challenge step.

--- courses/07-essential-javaScript/02-build-a-meme-app/index.js
+++ courses/07-essential-javaScript/02-build-a-meme-app/index.js
@@ -188,13 +188,15 @@ const catsData = [
 // }
 
 function getEmotionsArray(cats) {
-    const emotionArray = []
-    for ( let i = 0; i < cats.length; i++ ) {
-        for ( let j = 0; j < cats[i].emotionTags.length; j++ ) {
-            emotionArray.push(cats[i].emotionTags[j])
-        }
-    }
-    console.log(emotionArray)
+/*
+    Challenge:
+    1. Set up a "for of" in getEmotionsArray to iterate
+       over the data.
+    2. For now, just log out each cat object individually.
+    */
+   for (let cat of cats) {
+       console.log(cat)
+   }
 }
 
 getEmotionsArray(catsData)
