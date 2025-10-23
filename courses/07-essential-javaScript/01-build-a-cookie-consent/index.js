d856b16bffaea0eb7426d2bd34446d3dfa928024
Mike Sobhy <sobhy0101@gmail.com>
2025-10-23 09:38:23 +0300

    Add meme app project and update cookie consent text
    Added the 'Build a Meme App' project including HTML, CSS, JS, images, and README. Also updated the cookie consent modal text in the cookie consent project to include the user's email in the message.

--- courses/07-essential-javaScript/01-build-a-cookie-consent/index.js
+++ courses/07-essential-javaScript/01-build-a-cookie-consent/index.js
@@ -50,7 +50,7 @@ consentForm.addEventListener('submit', function(e){
       setTimeout(function(){
         document.getElementById('modal-inner').innerHTML = `
         <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
-        <p>We just sold the rights to your eternal soul.</p>
+        <p>We just sold the rights $${email} to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/pirate.gif">
         </div>
---
+++ courses/07-essential-javaScript/02-build-a-meme-app/README.md
@@ -0,0 +1,14 @@
+# Build A Meme App
+
+In this project, you'll build a simple meme generator app using JavaScript. The app will allow users to select a cat image or gif, add top and bottom text, and generate a meme.
+
+## Topics Covered
+
+- for of
+- radio & checkbox inputs
+- getElementsByClassName
+- querySelector
+- classList.remove
+- import/export
+- .includes()
+- .filter()
