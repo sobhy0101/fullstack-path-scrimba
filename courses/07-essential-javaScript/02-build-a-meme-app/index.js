98f51d442ac0ac599a75b1fdc21ddce8c5825b5a
Mike Sobhy <sobhy0101@gmail.com>
2025-10-24 11:10:46 +0300

    Refactor emotion array and render radio inputs
    Emotions are now only added to the array if not already present, preventing duplicates. The radio input rendering logic is implemented, replacing placeholder code with actual radio buttons and labels for each emotion.

--- courses/07-essential-javaScript/02-build-a-meme-app/index.js
+++ courses/07-essential-javaScript/02-build-a-meme-app/index.js
@@ -2,11 +2,22 @@ import { catsData } from "./data.js"
 
 const emotionRadios = document.getElementById("emotion-radios")
 
+/*
+Challenge:
+1. Refactor this nested for of so that an 
+   emotion is only pushed to emotionsArray
+   if it is not already in emotionsArray.
+   Extra kudos if you use the "logical not"
+   operator - feel free to google it!
+*/
+
 function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            emotionsArray.push(emotion)
+            if (!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
         }
     }
     return emotionsArray
@@ -16,22 +27,7 @@ function renderEmotionsRadios(cats){
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-
-/*
-Challenge:
-1. Swap out `<p>${emotion}</p>` for HTML
-   that will render a radio input for each
-   emotion. Remember to use "type", "id", 
-   "value", and "name" properties on each radio.
-   ("id" and "value" can both be set to the
-   "emotion").
-2. Remember to give each radio a label.
-   (What property does a label need?)
-3. Enclose each individual radio input in this div:
-
-*/ 
-
-        radioItems += `
+            radioItems += `
             <div class="radio">
                 <label for="${emotion}">${emotion}</label>
                 <input type="radio" id="${emotion}" name="emotion" value="${emotion}">
