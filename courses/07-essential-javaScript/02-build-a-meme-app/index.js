14422de9509f93df8407bafb440aded4fbc96079
Mike Sobhy <sobhy0101@gmail.com>
2025-10-24 10:29:06 +0300

    Render emotion options as radio inputs
    Replaced emotion text with radio input elements for each emotion, including appropriate label and input attributes, and wrapped each in a div for improved UI and accessibility.

--- courses/07-essential-javaScript/02-build-a-meme-app/index.js
+++ courses/07-essential-javaScript/02-build-a-meme-app/index.js
@@ -16,7 +16,27 @@ function renderEmotionsRadios(cats){
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+
+/*
+Challenge:
+1. Swap out `<p>${emotion}</p>` for HTML
+   that will render a radio input for each
+   emotion. Remember to use "type", "id", 
+   "value", and "name" properties on each radio.
+   ("id" and "value" can both be set to the
+   "emotion").
+2. Remember to give each radio a label.
+   (What property does a label need?)
+3. Enclose each individual radio input in this div:
+
+*/ 
+
+        radioItems += `
+            <div class="radio">
+                <label for="${emotion}">${emotion}</label>
+                <input type="radio" id="${emotion}" name="emotion" value="${emotion}">
+            </div>
+        `
     }
     emotionRadios.innerHTML = radioItems
 }
