f724dae1e501ce27312f561c18dd116b94962df2
Mike Sobhy <sobhy0101@gmail.com>
2025-10-26 08:56:56 +0300

    Add GIFs only option handling to meme app
    Introduced control for the 'gifs only' checkbox and added logic to store its checked state in a boolean within getMatchingCatsArray. The boolean is logged for verification.

--- courses/07-essential-javaScript/02-build-a-meme-app/index.js
+++ courses/07-essential-javaScript/02-build-a-meme-app/index.js
@@ -2,16 +2,24 @@ import { catsData } from "./data.js"
 
 const emotionRadios = document.getElementById("emotion-radios")
 const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
 
 getImageBtn.addEventListener('click', getMatchingCatsArray)
 
 function getMatchingCatsArray(){
 /*
 Challenge:
-1. Add code to getMatchingCatsArray so 
-   that the two existing lines of code 
-   only run if an emotion has been selected.
+1. Take control of the gifs only option checkbox. ✔️
+2. Set up a const in getMatchingCatsArray to store 
+   a boolean which will be set to true if the 
+   "gifs only" option is checked and false if it's
+   not. (Think what a good name for this const would 
+   be.)
+3. Log it out to check it's working.
 */
+    const isGif = gifsOnlyOption.checked
+    console.log(isGif)
+
     if (document.querySelector('input[type="radio"]:checked')) {
         const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
         console.log(selectedEmotion)
