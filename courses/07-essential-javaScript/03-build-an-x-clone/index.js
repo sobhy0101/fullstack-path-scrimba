3fc8d712d8693fffca2e95f714fa46fa5b841615
Mike Sobhy <sobhy0101@gmail.com>
2025-11-07 18:16:37 +0200

    Clean up getFeedHtml and remove comments
    Removed unnecessary console log and challenge comments from index.js. The feed is now rendered directly by calling getFeedHtml, improving code clarity.

--- courses/07-essential-javaScript/03-build-an-x-clone/index.js
+++ courses/07-essential-javaScript/03-build-an-x-clone/index.js
@@ -1,4 +1,4 @@
-import { tweetsData } from './data.js';
+import { tweetsData } from './data.js'
 
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
@@ -9,7 +9,7 @@ tweetBtn.addEventListener('click', function(){
 })
 
 function getFeedHtml(){
-    let feedHtml = ``;
+    let feedHtml = ``
 
     for (let tweet of tweetsData) {
         feedHtml += `
@@ -35,45 +35,7 @@ function getFeedHtml(){
         </div>
         `
     }
-
-    console.log(feedHtml);
-    return feedHtml;
+    return feedHtml
 }
 
-feedElement.innerHTML = getFeedHtml();
-/*
-Challenge:
-1. Use a "for of" to iterate over the data and 
-   create HTML string for each tweet using the 
-   boilerplate below. Replace UPPERCASE text
-   with data from the tweets. ✔️
-2. Store this HTML in a let called "feedHtml".
-3. Log out feedHtml. ✔️
-4. Call getFeedHtml to check it's working. ✔️
-*/  
-
-
-
-/*
-<div class="tweet">
-    <div class="tweet-inner">
-        <img src="URL OF PROFILE PIC" class="profile-pic">
-        <div>
-            <p class="handle">TWEET HANDLE</p>
-            <p class="tweet-text">TWEET TEXT</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                    NUMBER OF REPLIES
-                </span>
-                <span class="tweet-detail">
-                    NUMBER OF LIKES
-                </span>
-                <span class="tweet-detail">
-                    NUMBER OF RETWEETS
-                </span>
-            </div>   
-        </div>            
-    </div>
-</div>
-
-*/
+feedElement.innerHTML = getFeedHtml()
