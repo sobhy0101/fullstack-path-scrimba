f630b59b373793a787d62c7dc3cced83b901016a
Mike Sobhy <sobhy0101@gmail.com>
2025-11-07 21:47:37 +0200

    Add Font Awesome icons to tweet details
    Integrated Font Awesome via CDN in index.html and updated index.js to include relevant icon tags for comments, likes, and retweets in the tweet details. Also added CSS styling for Font Awesome icons to improve visual consistency.

--- courses/07-essential-javaScript/03-build-an-x-clone/index.js
+++ courses/07-essential-javaScript/03-build-an-x-clone/index.js
@@ -11,9 +11,18 @@ tweetBtn.addEventListener('click', function(){
 function getFeedHtml(){
     let feedHtml = ``
 
-    /*
+/*
 Challenge:
-1. Replace the for of with a forEach.
+1. Inside each span that has a class of "tweet-detail",
+   add an <i> tag.
+2. Give each <i> tag the classes it needs to render the
+   correct icons next to the numbers.
+   The classes you will need are:
+    fa-regular, 
+    fa-solid, 
+    fa-comment-dots, 
+    fa-heart, 
+    fa-retweet
 */
 
     tweetsData.forEach(function(tweet){
@@ -26,12 +35,15 @@ Challenge:
                     <p class="tweet-text">${tweet.tweetText}</p>
                     <div class="tweet-details">
                         <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots"></i>
                             ${tweet.replies.length} 
                         </span>
                         <span class="tweet-detail">
+                            <i class="fa-solid fa-heart"></i>
                             ${tweet.likes} 
                         </span>
                         <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet"></i>
                             ${tweet.retweets} 
                         </span>
                     </div>   
@@ -47,13 +59,5 @@ Challenge:
 
 function render(){
     document.getElementById('feed').innerHTML = getFeedHtml()
-/*
-Challenge:
-1. Take control of the ‘feed’ div.
-2. Render the HTML returned by the getFeedHtml 
-   function to the 'feed' div. 
-   See if you can do this with just one line of code!
-*/
 }
 render();
-// call render
