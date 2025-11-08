7ba59ba23997bed5b4d2c2b3fdcf9486aa68c382
Mike Sobhy <sobhy0101@gmail.com>
2025-11-08 09:04:44 +0200

    Add data attributes to tweet action icons
    Added 'data-reply', 'data-like', and 'data-retweet' attributes to the respective <i> tags in the tweet feed, each holding the tweet's uuid. This enables easier identification and handling of user interactions for replies, likes, and retweets.

--- courses/07-essential-javaScript/03-build-an-x-clone/index.js
+++ courses/07-essential-javaScript/03-build-an-x-clone/index.js
@@ -9,6 +9,14 @@ tweetBtn.addEventListener('click', function(){
 
 function getFeedHtml(){
     let feedHtml = ``
+/*
+Challenge:
+1. Add data attributes to each of the  <i> tags. You can call
+   them 'reply', 'like', and 'retweet’. ✔️
+2. Each data attribute should hold the tweet's uuid. ✔️
+*/
+
+
     tweetsData.forEach(function(tweet){
         feedHtml += `
         <div class="tweet">
@@ -19,15 +27,15 @@ function getFeedHtml(){
                     <p class="tweet-text">${tweet.tweetText}</p>
                     <div class="tweet-details">
                         <span class="tweet-detail">
-                            <i class="fa-regular fa-comment-dots"></i>
+                            <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
                             ${tweet.replies.length} 
                         </span>
                         <span class="tweet-detail">
-                            <i class="fa-solid fa-heart"></i>
+                            <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
                             ${tweet.likes} 
                         </span>
                         <span class="tweet-detail">
-                            <i class="fa-solid fa-retweet"></i>
+                            <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
                             ${tweet.retweets} 
                         </span>
                     </div>   
