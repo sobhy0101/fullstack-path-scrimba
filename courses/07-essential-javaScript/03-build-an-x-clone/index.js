90ae4d39c2a40c2a9708a49a23d7fec5b67a01d1
Mike Sobhy <sobhy0101@gmail.com>
2025-11-09 14:36:06 +0200

    Completed the X Clone App
    Moved the render() call to the end of the file for better code organization. Improved code formatting and consistency by updating spacing, indentation, and function definitions throughout index.js.

--- courses/07-essential-javaScript/03-build-an-x-clone/index.js
+++ courses/07-essential-javaScript/03-build-an-x-clone/index.js
@@ -1,61 +1,55 @@
 import { tweetsData } from './data.js'
 import { v4 as uuidv4 } from 'https://cdn.jsdelivr.net/npm/uuid@9.0.1/dist/esm-browser/index.js'
 
-/*
-Challenge:
-We could improve index.js by moving one line
-   of code to a better position. Find it and move it!
-*/
-
-document.addEventListener('click', function(e){
-    if(e.target.dataset.like){
-       handleLikeClick(e.target.dataset.like) 
+document.addEventListener('click', function (e) {
+    if (e.target.dataset.like) {
+        handleLikeClick(e.target.dataset.like)
     }
-    else if(e.target.dataset.retweet){
+    else if (e.target.dataset.retweet) {
         handleRetweetClick(e.target.dataset.retweet)
     }
-    else if(e.target.dataset.reply){
+    else if (e.target.dataset.reply) {
         handleReplyClick(e.target.dataset.reply)
-    } else if(e.target.id === 'tweet-btn'){
+    } else if (e.target.id === 'tweet-btn') {
         handleTweetBtnClick()
     }
 })
- 
-function handleLikeClick(tweetId){ 
-    const targetTweetObj = tweetsData.filter(function(tweet){
+
+function handleLikeClick(tweetId) {
+    const targetTweetObj = tweetsData.filter(function (tweet) {
         return tweet.uuid === tweetId
     })[0]
 
-    if (targetTweetObj.isLiked){
+    if (targetTweetObj.isLiked) {
         targetTweetObj.likes--
     }
-    else{
-        targetTweetObj.likes++ 
+    else {
+        targetTweetObj.likes++
     }
     targetTweetObj.isLiked = !targetTweetObj.isLiked
     render()
 }
 
-function handleRetweetClick(tweetId){
-    const targetTweetObj = tweetsData.filter(function(tweet){
+function handleRetweetClick(tweetId) {
+    const targetTweetObj = tweetsData.filter(function (tweet) {
         return tweet.uuid === tweetId
     })[0]
-    
-    if(targetTweetObj.isRetweeted){
+
+    if (targetTweetObj.isRetweeted) {
         targetTweetObj.retweets--
     }
-    else{
+    else {
         targetTweetObj.retweets++
     }
     targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
-    render() 
+    render()
 }
 
-function handleReplyClick(replyId){
+function handleReplyClick(replyId) {
     document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
 }
 
-function handleTweetBtnClick(){
+function handleTweetBtnClick() {
     const tweetInput = document.getElementById('tweet-input')
     const tweetText = tweetInput.value
 
@@ -78,28 +72,28 @@ function handleTweetBtnClick(){
     tweetInput.value = ''
 }
 
-function getFeedHtml(){
+function getFeedHtml() {
     let feedHtml = ``
-    
-    tweetsData.forEach(function(tweet){
-        
+
+    tweetsData.forEach(function (tweet) {
+
         let likeIconClass = ''
-        
-        if (tweet.isLiked){
+
+        if (tweet.isLiked) {
             likeIconClass = 'liked'
         }
-        
+
         let retweetIconClass = ''
-        
-        if (tweet.isRetweeted){
+
+        if (tweet.isRetweeted) {
             retweetIconClass = 'retweeted'
         }
-        
+
         let repliesHtml = ''
-        
-        if(tweet.replies.length > 0){
-            tweet.replies.forEach(function(reply){
-                repliesHtml+=`
+
+        if (tweet.replies.length > 0) {
+            tweet.replies.forEach(function (reply) {
+                repliesHtml += `
 <div class="tweet-reply">
     <div class="tweet-inner">
         <img src="${reply.profilePic}" class="profile-pic">
@@ -112,8 +106,8 @@ function getFeedHtml(){
 `
             })
         }
-        
-          
+
+
         feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
@@ -148,13 +142,12 @@ function getFeedHtml(){
     </div>   
 </div>
 `
-   })
-   return feedHtml 
+    })
+    return feedHtml
 }
 
-function render(){
+function render() {
     document.getElementById('feed').innerHTML = getFeedHtml()
 }
 
 render()
-
