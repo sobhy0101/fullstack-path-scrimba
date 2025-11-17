8275fbb0bdef77ca1a85ed888dfa045891a58d5a
Mike Sobhy <sobhy0101@gmail.com>
2025-11-17 21:58:37 +0200

    Add .map() method playlist mini project
    Introduced a new mini project demonstrating the use of the JavaScript .map() method to render a playlist. Added all supporting files including HTML, CSS, images, playlist data, and a README for setup instructions.

---
+++ courses/07-essential-javaScript/04-mini-projects/the-.map-method-challenge/index.js
@@ -0,0 +1,26 @@
+import { playlistArr } from './playlist.js'
+
+/*
+Challenge
+1. Refactor the code below to use .map() 
+   instead of the for loop.
+   ⚠️ Don't worry about the commas for now.
+*/
+
+const playlistHtml = playlistArr.map(item =>
+`<section class="card">
+    <div class="card-start">
+        <img src="./images/${item.albumArt}">
+    </div>
+        <div class="card-mid">
+            <h4 class="card-title">${item.title}</h4>
+            <p class="card-artist">${item.artist}</p>
+        </div>
+    <div class="card-end">
+        <p class="card-menu">...</p>
+    </div>
+</section>
+`
+)
+.join('')
+document.getElementById('container').innerHTML = playlistHtml
