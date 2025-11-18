3393af980c4c4f161ce4972ccb6112e5a7f8ff4b
Mike Sobhy <sobhy0101@gmail.com>
2025-11-18 19:20:40 +0200

    Refactor playlist rendering and update container styling
    Refactored the playlist rendering logic to use a named function in the map method for improved readability. Updated CSS to center the container, set its width, and change flex direction to column for better layout alignment.

--- courses/07-essential-javaScript/04-mini-projects/the-.map-method-challenge/index.js
+++ courses/07-essential-javaScript/04-mini-projects/the-.map-method-challenge/index.js
@@ -7,21 +7,43 @@ Challenge
    ⚠️ Don't worry about the commas for now.
 */
 
-const playlistHtml = playlistArr.map(item =>
-`<section class="card">
+const playlistHtml = playlistArr.map(function (track) {
+    return `
+<section class="card">
     <div class="card-start">
-        <img src="./images/${item.albumArt}">
+        <img src="images/${track.albumArt}">
     </div>
         <div class="card-mid">
-            <h4 class="card-title">${item.title}</h4>
-            <p class="card-artist">${item.artist}</p>
+            <h4 class="card-title">${track.title}</h4>
+            <p class="card-artist">${track.artist}</p>
         </div>
     <div class="card-end">
         <p class="card-menu">...</p>
     </div>
 </section>
 `
-)
+})
 .join('')
+
+// Using the old for each loop
+// 
+// const playlistHtml = []
+// for (let i = 0; i < playlistArr.length; i++) {
+//     playlistHtml.push(
+// `<section class="card">
+//     <div class="card-start">
+//         <img src="/images/${playlistArr[i].albumArt}">
+//     </div>
+//         <div class="card-mid">
+//             <h4 class="card-title">${playlistArr[i].title}</h4>
+//             <p class="card-artist">${playlistArr[i].artist}</p>
+//         </div>
+//     <div class="card-end">
+//         <p class="card-menu">...</p>
+//     </div>
+// </section>
+//     `
+//     )
+// }
 
 document.getElementById('container').innerHTML = playlistHtml
