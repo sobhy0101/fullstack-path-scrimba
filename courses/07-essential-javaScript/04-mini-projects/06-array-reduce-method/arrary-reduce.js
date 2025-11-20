6e6da2ab5ee78908a629b12d0d3070f93819f896
Mike Sobhy <sobhy0101@gmail.com>
2025-11-20 23:48:24 +0200

    Add Array.reduce() mini project and footer
    Introduced a new mini project demonstrating the Array.reduce() method with supporting HTML and JS files. Updated the mini-projects index to include the new project and added a footer section with corresponding styles.

---
+++ courses/07-essential-javaScript/04-mini-projects/06-array-reduce-method/arrary-reduce.js
@@ -0,0 +1,13 @@
+const rainJanuaryByWeek = [ 10, 20, 0, 122 ]
+
+const totalRainfallJanuary = rainJanuaryByWeek.reduce(function(total, currentElement){
+    console.log('total: ' + total, 'currentElement: ' + currentElement)
+    return total + currentElement
+})
+
+console.log(totalRainfallJanuary)
+
+// total: 10,"currentElement: 20"
+// ›total: 30,"currentElement: 0"
+// ›total: 30,"currentElement: 122"
+// ›152
