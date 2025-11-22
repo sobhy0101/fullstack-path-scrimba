a6221e322ca6745f2ef36771affb3148f12cc1f5
Mike Sobhy <sobhy0101@gmail.com>
2025-11-22 10:57:51 +0200

    Fix import path and implement reduce logic
    Corrects the import path for itemsBoughtArr and completes the calculateTotalCost function using Array.reduce to sum item prices. This ensures the total cost is properly calculated and logged.

--- courses/07-essential-javaScript/04-mini-projects/06-array-reduce-method/the-.reduce-method-with-objects-challenge-#1/index.js
+++ courses/07-essential-javaScript/04-mini-projects/06-array-reduce-method/the-.reduce-method-with-objects-challenge-#1/index.js
@@ -1,13 +1,17 @@
-import { itemsBoughtArr } from '/itemsBoughtArr.js'
+import { itemsBoughtArr } from './itemsBoughtArr.js'
 
 function calculateTotalCost(itemsBoughtArr){
+
+    const total = itemsBoughtArr.reduce((total, currentItem)=>
+        total + currentItem.priceUSD, 0
+        )
+    return total
+}
+
+console.log(calculateTotalCost(itemsBoughtArr))
+
 /*
 Challenge:
 1. Use the reduce method to calculate the total 
    cost of items which have been bought.
 */
-    const total = 
-    return total
-}
-
-console.log(calculateTotalCost(itemsBoughtArr))
