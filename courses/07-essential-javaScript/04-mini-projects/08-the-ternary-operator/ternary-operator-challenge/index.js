4951573d38c607309ebe4112f45112c7b2d8bf5b
Mike Sobhy <sobhy0101@gmail.com>
2025-11-23 13:11:29 +0200

    Improve ternary operator challenge feedback
    Enhanced the ternary operator challenge to provide more detailed feedback: the player is now informed if their guess is too high, too low, or exactly right. Also updated HTML metadata and script loading for better standards compliance.

--- courses/07-essential-javaScript/04-mini-projects/08-the-ternary-operator/ternary-operator-challenge/index.js
+++ courses/07-essential-javaScript/04-mini-projects/08-the-ternary-operator/ternary-operator-challenge/index.js
@@ -1,17 +1,13 @@
-const playerGuess = 3
+const playerGuess = 8
 const correctAnswer = 6
 
 /*
-Challenge 
-1. Refactor the if else statement to use a ternary operator.
+Challenge #2
+1. Now improve the functionality of this code by 
+   letting the player know if their guess was too high, 
+   too low, or exactly right.
 */
 
-let message = ''
-if (playerGuess === correctAnswer) {
-    message = 'Correct!'
-}
-else {
-    message = 'Wrong!'
-}
+const message = playerGuess === correctAnswer ? 'Exactly right!' : playerGuess > correctAnswer ? 'Too high!' : 'Too low!'
 
 console.log(message)
