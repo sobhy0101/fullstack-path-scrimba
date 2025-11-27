5c1b9ba57275cadf932c3db9b5587adda05b3088
Mike Sobhy <sobhy0101@gmail.com>
2025-11-27 10:42:34 +0200

    Rename and enhance Error constructor mini-project
    Renamed the '15-constructor-error' folder to '15-constructors-error' and updated all related files. Improved the content and styling of the mini-project, added more detailed explanations and examples in the HTML, enhanced the CSS, and updated the JavaScript to demonstrate error handling. Updated the project listing in the main index.html to reflect these changes.

--- courses/07-essential-javaScript/04-mini-projects/15-constructors-error/index.js
+++ courses/07-essential-javaScript/04-mini-projects/15-constructors-error/index.js
@@ -3,9 +3,11 @@ function checkUsername(userName) {
         console.log(userName)
     } else {
         console.log('I execute')
+        // This line will not execute because the error is thrown above
+        // The throw statement creates a new Error object and stops the function execution
         throw new Error('No username provided')
         console.log('I do not execute')
     }
 }
 
-checkUsername()
+checkUsername('admin')
