c991491fd2bade504ffca18674e327d2c0411dbc
Mike Sobhy <sobhy0101@gmail.com>
2025-11-29 11:41:06 +0200

    Completed Debugging all errors
    Debugged the event listener assignment for the check button and fixed the age comparison logic to handle all age cases properly. Also updated variable names for consistency and added useful debugging references as comments.

--- courses/07-essential-javaScript/04-mini-projects/19-debugging-errors/index.js
+++ courses/07-essential-javaScript/04-mini-projects/19-debugging-errors/index.js
@@ -7,24 +7,20 @@ const minDrinkAge = 21
 const minDriveAge = 16
 
 //check button click event listener
-checkButton.value.addEventListener('click', function () {
+checkButton.addEventListener('click', function () {
     let message = ''
-    const age = AgeInput.value
-        // Age is below the minDrinkAge and minDriveAge
-        if (age > minDrinkAge && age > minDriveAge) {
-            message = `I'm sorry, you cannot drink or drive ⛔`
-        // Relevant to countries where minDrinkAge is less than minDriveAge
-        } else if (age >= minDrinkAge && age < minDriveAge) {
-            message = "You can drink 🍺 but you cannot drive"
-        // Relevant to countries where minDriveAge is less than minDrinkAge
-        } else if (age >= minDriveAge && age < minDrinkAge) {
-            message = "You can drive 🚗 but you cannot drink"
-        // Age is above the minDrinkAge and minDriveAge
-        } else {
-            message = "You can drink 🍺 and drive 🚗 (not at the same time though!)"
-        } 
-        renderMessage(message)
-}
+    const age = Number(ageInput.value)
+    if (age < minDriveAge && age < minDrinkAge) {
+        message = `I'm sorry, you cannot drink or drive ⛔`
+    } else if (age >= minDrinkAge && age < minDriveAge) {
+        message = "You can drink 🍺 but you cannot drive"
+    } else if (age >= minDriveAge && age < minDrinkAge) {
+        message = "You can drive 🚗 but you cannot drink"
+    } else if (age >= minDriveAge && age >= minDrinkAge) {
+        message = "You can drink 🍺 and drive 🚗 (not at the same time though!)"
+    }
+    renderMessage(message)
+})
 
 function renderMessage(message) {
     resultDisplay.innerText = message
@@ -37,3 +33,7 @@ function renderMessage(message) {
 // 4. Check for common mistakes (typos, missing brackets, incorrect variable names, etc.)
 // 5. Use console.log() to trace variable values and program flow
 // 6. Test your code after making changes to ensure the error is resolved
+// Referance: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Debugging
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors
+// Reference: https://www.freecodecamp.org/news/how-to-debug-javascript-in-2020-8f1a6c3b3b6d/
+// Reference: https://javascript.info/debugging-chrome
