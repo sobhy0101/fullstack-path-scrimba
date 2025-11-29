88cf912445180cdec15c65fe52ef689559ddee9a
Mike Sobhy <sobhy0101@gmail.com>
2025-11-29 10:15:24 +0200

    Enhance JS class challenge and update styles
    Added a second Character instance and more item additions in the constructor-functions-to-classes challenge. Updated HTML to use a new 'space' class for spacing, and added corresponding CSS. Minor formatting improvements in related CSS files.

--- courses/07-essential-javaScript/04-mini-projects/18-constructor-functions-to-classes/constructor-functions-to-classes-challenge/index.js
+++ courses/07-essential-javaScript/04-mini-projects/18-constructor-functions-to-classes/constructor-functions-to-classes-challenge/index.js
@@ -9,11 +9,19 @@ class Character {
         this.name = name
         this.collectedItemsArr = []
     }
-    addItem(item) {
+    
+    addItem(item){
         this.collectedItemsArr.push(item)
-        console.log(`${this.name} now has: ${this.collectedItemsArr.join(', ')}`)
+        console.log(`${this.name} now has: ${this.collectedItemsArr.join(', ')}`)    
     }
 }
 
 const wizard = new Character('Merlin')
+const witch = new Character('Hermione')
 wizard.addItem('a wand')
+wizard.addItem('a map')
+wizard.addItem('a rope')
+wizard.addItem('a cauldron')
+witch.addItem('a map')
+witch.addItem('a rope')
+witch.addItem('a cauldron')
