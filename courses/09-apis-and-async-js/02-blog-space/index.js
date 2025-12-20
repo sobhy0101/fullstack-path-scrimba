606203aed94b2917a5a0fb386f0bb8489371117b
Mike Sobhy <sobhy0101@gmail.com>
2025-12-20 11:09:28 +0200

    Update blog form fields and add submit handler
    Refactored the blog form in index.html to use new input IDs and simplified labels. Added a submit event listener in index.js to prevent default form submission, collect input values, and log the post object to the console.

--- courses/09-apis-and-async-js/02-blog-space/index.js
+++ courses/09-apis-and-async-js/02-blog-space/index.js
@@ -12,3 +12,26 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
         }
         document.getElementById("blog-list").innerHTML = html
     })
+
+/**
+ Challenge:
+ 
+ * Listen for the "submit" event on the form (which will happen when the button is clicked)
+    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
+ * Combine the title value and body value into an object (with a "title" property and "body" property)
+ * Log the object to the console
+
+*/
+
+const blogForm = document.getElementById("blog-form")
+
+blogForm.addEventListener("submit", function(event) {
+    event.preventDefault()
+    const title = document.getElementById("post-title").value
+    const body = document.getElementById("post-body").value
+    const post = {
+        title: title,
+        body: body
+    }
+    console.log(post)
+})
