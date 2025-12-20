adc3e3d1b8c38cb01847f63719577b61ae085a9b
Mike Sobhy <sobhy0101@gmail.com>
2025-12-20 16:30:27 +0200

    Update blog form ID and handle new post submission
    Changed the blog form's ID in index.html to 'new-post' to match the event listener in index.js. Improved the post submission logic to send a POST request with the new blog entry and update the DOM with the new post at the top of the blog list.

--- courses/09-apis-and-async-js/02-blog-space/index.js
+++ courses/09-apis-and-async-js/02-blog-space/index.js
@@ -5,7 +5,7 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
         let html = ""
         for (let post of postsArr) {
             html += `
-                <h3>${post.title}</h3>
+                <h3 class="blah">${post.title}</h3>
                 <p>${post.body}</p>
                 <hr />
             `
@@ -21,22 +21,26 @@ document.getElementById("new-post").addEventListener("submit", function(e) {
         title: postTitle,
         body: postBody
     }
-    /**
-     * Challenge: Send this off to the server!
-     * 
-     * 1. BaseURL: https://apis.scrimba.com/jsonplaceholder/
-     * 2. Endpoint: /posts
-     * 3. method: ???
-     * 4. Request body: ??? (Remember to turn it into JSON)
-     * 5. Headers: ??? (Check the JSON Placeholder API docs or past casts for help)
-     */
-    fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+    
+    const options = {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
             "Content-Type": "application/json"
         }
-    })
+    }
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(post => {
+            /**
+             * Challenge: Update the DOM with the new blog entry
+             */
+            document.getElementById("blog-list").innerHTML = `
+                <h3 class="blah">${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+                ${document.getElementById("blog-list").innerHTML}
+            `
+        })
 })
