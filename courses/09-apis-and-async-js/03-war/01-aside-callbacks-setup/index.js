b64c3295438f555453103790c85bff8b83cfcd06
Mike Sobhy <sobhy0101@gmail.com>
2025-12-31 12:06:29 +0200

    Add filter and map chaining to extract voter emails
    Implemented method chaining to filter voters who voted and map their email addresses. Also included both traditional and arrow function syntax examples for educational purposes.

--- courses/09-apis-and-async-js/03-war/01-aside-callbacks-setup/index.js
+++ courses/09-apis-and-async-js/03-war/01-aside-callbacks-setup/index.js
@@ -60,14 +60,38 @@
 /**
  * Challenge: method chaining!
  * 
- * 1. Select the button in the DOM and add an event listener to it without
- *    saving the DOM element as a separate variable. I.e. "chain" the `addEventListener`
- *    on after your `getElementById()`(When clicked, log "Clicked" to the console)
+ * 1. Select the button in the DOM and add an event listener to it without saving the DOM element as a separate variable. I.e. "chain" the `addEventListener` on after your `getElementById()`(When clicked, log "Clicked" to the console)
  *    - I realize this might feel like busywork, but my intent will make sense soon
  * 
- * 2. Upcoming...
+ * 2. Given the array below, chain the `.filter` and `.map` array methods together to turn the array into an array of string email addresses of only the people in the array who voted. Log the array of email addresses to the console
  */
 
-document.getElementById("new-deck").addEventListener("click", function() {
-    console.log("Clicked")
-})
+// document.getElementById("new-deck").addEventListener("click", function() {
+//     console.log("Clicked!")
+// })
+
+const voters = [
+    {name: "Joe", email: "joe@joe.com", voted: true},
+    {name: "Jane", email: "jane@jane.com", voted: true},
+    {name: "Bo", email: "bo@bo.com", voted: false},
+    {name: "Bane", email: "bane@bane.com", voted: false}
+]
+
+// Write your code below
+
+
+// Final result: ["joe@joe.com", "jane@jane.com"]
+
+// Using traditional function syntax
+    // const voterEmails = voters
+    //     .filter(function(voter) {
+    //         return voter.voted
+    //     })
+    //     .map(function(voter) {
+    //         return voter.email
+    //     })
+
+// Using arrow function syntax
+    const voterEmails = voters.filter(voter => voter.voted).map(voter => voter.email)
+
+console.log(voterEmails)
