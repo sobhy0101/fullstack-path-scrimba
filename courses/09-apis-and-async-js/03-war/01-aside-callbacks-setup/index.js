f44d1c90c530f341ac583c0aa60185459ec6c2ed
Mike Sobhy <sobhy0101@gmail.com>
2026-01-02 17:15:16 +0200

    Add card winner determination logic
    Introduced the determineWinner function to compare two card values and log which card wins or if it's a tie. This enhances the card drawing feature by providing immediate feedback on the winning card.

--- courses/09-apis-and-async-js/03-war/01-aside-callbacks-setup/index.js
+++ courses/09-apis-and-async-js/03-war/01-aside-callbacks-setup/index.js
@@ -23,5 +23,50 @@ drawCardBtn.addEventListener("click", () => {
             cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
+            determineWinner(data.cards[0], data.cards[1])
         })
 })
+
+/**
+ * Challenge:
+ * 
+ * Try to determine which of the 2 cards is the "winner" (has higher value)
+ * Aces are the card with the highest "score"
+ * 
+ * In parts:
+ * 
+ * 1. Create a function that takes 2 card objects as parameters, 
+ * `card1` and `card2`. These card objects have a property called
+ * `value`, which can be any one of the following strings, in
+ * order of rising "score":
+ * 
+ * "2", "3", "4", "5", "6", "7", "8", "9", 
+ * "10", "JACK", "QUEEN", "KING", "ACE"
+ * 
+ * I.e. "2" is the lowest score and "ACE" is the highest.
+ * 
+ * The function should determine which of the 2 cards (`card1`
+ * or `card2`) has the higher score, or if they have the same score.
+ * 
+ * Log which card wins (or "It's a tie!" 
+ * if they're the same) to the console
+ */
+
+function determineWinner(card1, card2) {
+    const valueOrder = [
+        "2", "3", "4", "5", "6", "7", "8", "9",
+        "10", "JACK", "QUEEN", "KING", "ACE"
+    ]
+
+    const card1ValueIndex = valueOrder.indexOf(card1.value)
+    const card2ValueIndex = valueOrder.indexOf(card2.value)
+
+    if (card1ValueIndex > card2ValueIndex) {
+        console.log("Card 1 wins!")
+    } else if (card2ValueIndex > card1ValueIndex) {
+        console.log("Card 2 wins!")
+    } else {
+        console.log("It's a tie!")
+    }
+}
+
