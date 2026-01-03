8911c724db6bbda292d237b183e691259efb2176
Mike Sobhy <sobhy0101@gmail.com>
2026-01-03 13:04:42 +0200

    Display winner message above cards in War game
    Added an h2 element to show the winner message above the cards. Updated styles for improved appearance and adjusted card slot sizes. Refactored winner determination logic to return a string and display it in the UI instead of logging to the console.

--- courses/09-apis-and-async-js/03-war/01-aside-callbacks-setup/index.js
+++ courses/09-apis-and-async-js/03-war/01-aside-callbacks-setup/index.js
@@ -17,13 +17,15 @@ drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
+            document.getElementById("winner-message").textContent = determineCardWinner(data.cards[0], data.cards[1])
+
             cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
             cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
-            determineWinner(data.cards[0], data.cards[1])
+            determineCardWinner(data.cards[0], data.cards[1])
         })
 })
 
@@ -33,40 +35,40 @@ drawCardBtn.addEventListener("click", () => {
  * Try to determine which of the 2 cards is the "winner" (has higher value)
  * Aces are the card with the highest "score"
  * 
- * In parts:
+ * Part 2:
+ * Instead of logging the winner to the console, 
+ * display an `h2` on the screen above the 2 cards 
+ * that declares who the winner is.
  * 
- * 1. Create a function that takes 2 card objects as parameters, 
- * `card1` and `card2`. These card objects have a property called
- * `value`, which can be any one of the following strings, in
- * order of rising "score":
- * 
- * "2", "3", "4", "5", "6", "7", "8", "9", 
- * "10", "JACK", "QUEEN", "KING", "ACE"
- * 
- * I.e. "2" is the lowest score and "ACE" is the highest.
- * 
- * The function should determine which of the 2 cards (`card1`
- * or `card2`) has the higher score, or if they have the same score.
- * 
- * Log which card wins (or "It's a tie!" 
- * if they're the same) to the console
+ * If card1 is the higher card, display "Computer wins!"
+ * If card2 is the higher card, display "You win!"
+ * If they're equal, display "War!"
  */
 
-function determineWinner(card1, card2) {
-    const valueOrder = [
-        "2", "3", "4", "5", "6", "7", "8", "9",
-        "10", "JACK", "QUEEN", "KING", "ACE"
-    ]
-
-    const card1ValueIndex = valueOrder.indexOf(card1.value)
-    const card2ValueIndex = valueOrder.indexOf(card2.value)
-
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+    console.log("card 1:", card1ValueIndex)
+    console.log("card 2:", card2ValueIndex)
+    
     if (card1ValueIndex > card2ValueIndex) {
-        console.log("Card 1 wins!")
-    } else if (card2ValueIndex > card1ValueIndex) {
-        console.log("Card 2 wins!")
+        return "Computer wins!"
+    } else if (card1ValueIndex < card2ValueIndex) {
+        return "You win!"
     } else {
-        console.log("It's a tie!")
+        return "War!"
     }
 }
+
+// Keeping this test case for reference, but should delete later
+// const card1Obj = {
+//     value: "JACK"
+// }
+// const card2Obj = {
+//     value: "QUEEN"
+// }
+
+// determineCardWinner(card1Obj, card2Obj)
 
