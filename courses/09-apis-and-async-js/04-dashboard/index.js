24346b7ff83eae6a03e3caa351540b2084674127
Mike Sobhy <sobhy0101@gmail.com>
2026-01-14 09:43:27 +0200

    Remove challenge instructions from index.js
    Deleted commented instructions related to displaying the image's author, leaving only the fetch logic in the file.

--- courses/09-apis-and-async-js/04-dashboard/index.js
+++ courses/09-apis-and-async-js/04-dashboard/index.js
@@ -1,13 +1,3 @@
-/**
- * Challenge part 2: Display the image's author
- * 
- * With the data you've already fetched, display the name of the 
- * image author on the page. They show up as the "user" in the data
- * returned from the API.
- * 
- * Don't worry about positioning the author in the lower-left yet.
- */
-
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=scifi")
     .then(res => res.json())
     .then(data => {
