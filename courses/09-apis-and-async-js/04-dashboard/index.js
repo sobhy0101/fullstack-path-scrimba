61443b8103764cd7693b0098b1c6f791525cd2f4
Mike Sobhy <sobhy0101@gmail.com>
2026-01-14 10:01:18 +0200

    Set default background image on Unsplash fetch error
    Changed the Unsplash API query to 'nature' and added error handling to set a default background image and author if the fetch fails. Also logs the fetched image URL to the console for debugging.

--- courses/09-apis-and-async-js/04-dashboard/index.js
+++ courses/09-apis-and-async-js/04-dashboard/index.js
@@ -1,9 +1,26 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=scifi")
+/* eslint-disable no-unused-vars */
+
+// Fetch a random nature image from Unsplash API
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
+        console.log(data.urls.regular)
         document.body.style.backgroundImage = `url(${data.urls.regular})`
-        document.getElementById("author").textContent = `By: ${data.user.name}`
+		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+    .catch(err => {
+        /**
+         * Challenge: get a URL for a default background image and set it here
+         * 
+         * 1. Change the query in the URL above back to something real ✅
+         * 2. Log the image url to the console (replacing console.log(data) above) ✅
+         * 3. Use that URL as the "default" background image to be used if 
+         *    the promise is ever rejected.
+         */
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1475598322381-f1b499717dda?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NjgzNzcxNjZ8&ixlib=rb-4.1.0&q=80&w=1080)`;
+        document.getElementById("author").textContent = `By: John Towner`;
+    })
+
 
 // Update time and date every minute
 function updateTimeAndDate() {
